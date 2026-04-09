3410610076817ee0c5940f09e4861c55a6b41199
Rinalon <124454184+Rinalon@users.noreply.github.com>
2026-04-09 12:37:54 +0300

    Update user_commands.py

--- Bots/Telegram/source/user_commands.py
+++ Bots/Telegram/source/user_commands.py
@@ -1,9 +1,11 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from source.stations import WorkStates
+
+import source.keyboards as kb
 
 router = Router()
 
@@ -32,3 +34,15 @@ async def help(message: Message, state: FSMContext):
         help_txt += "\n\nНа данный момент у вас нет ни одного подключённого хаба. Воспользуйтесь командой /add."
 
     await message.answer(help_txt)
+
+@router.callback_query(F.data.startswith("page_"))
+async def change_sensor_page(callback: CallbackQuery, state: FSMContext):
+    page = int(callback.data.split("_")[1])
+    data = await state.get_data()
+    hub_id = data.get('hub_id')
+    if hub_id is None:
+        await callback.answer("Ошибка: не найден хаб", show_alert=True)
+        return
+    keyboard = await kb.choose_sensor(hub_id, page=page)
+    await callback.message.edit_reply_markup(reply_markup=keyboard)
+    await callback.answer()
